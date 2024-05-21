9e76056a197c76f2e02eae68302697e4a206d2c0
Robert Hlobik <roberthlobik@gmail.com>
2024-05-21 18:20:41 +0200

    Nadopuna 2

---
+++ 3.Operatori/increment_decrement.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$a++; // Ekvivalent $a +1;
+++$b; // 21
+
+echo $b;//21
+echo '<br>';
+
+echo $b--;//21
+echo '<br>';
+
+echo --$b;//19
+
+
+// prednost pri izvodjenju izraza
+// ()
+// !
+// */
+// %
+// +-
+// <> <= >=
+// !== !=
+// &&
+// ||git 
