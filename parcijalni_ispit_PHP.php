2bd191ee255367571dbcfb547ea1cea4e0b7b431
Robert Hlobik <roberthlobik@gmail.com>
2024-06-07 12:55:49 +0200

    Ispravak parcijalnog_ispita_PHP

--- parcijalni_ispit_PHP.php
+++ parcijalni_ispit_PHP.php
@@ -1,47 +1,36 @@
 <?php
+session_start();
 
 const FILE_PATH = __DIR__ . '/words.json';
 
 $words = file_get_contents(FILE_PATH);
 
-var_dump($words);
-
 $words = json_decode($words, true);
 
-var_dump($words);
-
-if(isset($_GET['sendWord'])){
-    if(empty($_GET['word'])){
-        echo "Upisite neku vrijednost";
+if(isset($_POST['sendWord'])){
+    if(empty($_POST['word'])){
+        $_SESSION['errors'] = 'Polje ne smije biti prazno!';
     }else{
-        $wordLen = mb_strlen($_GET['word']);
-        echo $wordLen;
-        $samo = izbrojiSamoglasnike($_GET['word']);
-        echo $samo;
-        $su = $wordLen - $samo;
-        $$words = [
-            "word" => $_GET['word'],
+        $wordLen = mb_strlen($_POST['word']);
+        $vowels= countVolwes($_POST['word']);
+        $consonants = $wordLen - $bravowels;
+        $words[] = [
+            "word" => $_POST['word'],
             "wordLen" => $wordLen,
-            "su" => $su,
-            "samo" => $samo
+            "consonants" => $consonants,
+            "vowels" => $vowels
         ];
         $words = json_encode($words);
         file_put_contents(FILE_PATH, $words);
+        header("Location: parcijalni_ispit_PHP.php");
     }
 }
 
-
-
-
-function izbrojiSamoglasnike($word) {
+function countVolwes($word) {
     $pattern = '/[aeiouAEIOU]/';
     preg_match_all($pattern, $word, $matches);
     return count($matches[0]);
 }
-
-
-
-
 
 ?>
 <!DOCTYPE html>
@@ -49,29 +38,41 @@ function izbrojiSamoglasnike($word) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Parcijalni ispit PHP</title>
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <form action="parcijalni_ispit_PHP.php" method="get">
-        <label for="word">Upišite riječ</label>    
-        <input type="text" name="word">
-        <input type="submit" name="sendWord" value="Pošalji">
-    </form>
-    <table border="1">
-        <tbody >
-            <tr>
-                <php
-                    if(){
-                        foreach
-                        <th>Riječ</th>
-                        <th>Broj slova</th>
-                        <th>Broj suglasnika</th>
-                        <th>Broj samoglasnika</th>
+    <main>
+        <form action="parcijalni_ispit_PHP.php" method="POST">
+            <label for="word"><?=$_SESSION['errors'] ?? 'Upišite riječ'?></label>    
+            <input type="text" name="word">
+            <input type="submit" name="sendWord" value="Pošalji">
+        </form>
+        <table border="1">
+            <tbody >
+                <tr>
+                    <th>Riječ</th>
+                    <th>Broj slova</th>
+                    <th>Broj suglasnika</th>
+                    <th>Broj samoglasnika</th>
+                </tr>
+                <?php
+                    if(is_array($words)){
+                        foreach($words as $k => $w){?>
+                            <tr>
+                                <td><?=$w['word']?></td>
+                                <td><?=$w['wordLen']?></td>
+                                <td><?=$w['consonants']?></td>
+                                <td><?=$w['vowels']?></td>
+                            </tr>
+                        <?php    
+                        }
                     }
                 ?>
-            </tr>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
+
+<?php session_unset(); ?>
